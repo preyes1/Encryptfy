01c5b871a31d2a52a790b62479fdf5063e388d8e
Paolo Reyes <paoloreyes@mac.home>
2024-11-23 15:19:32 -0500

    added information in home page

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def home():
-    return render_template('index.html') 
-    
+    return render_template('index.html', active_page="home") 
 
 @app.route('/encrypt', methods=["POST", "GET"])
 def encrypt():
@@ -18,12 +17,13 @@ def encrypt():
 
         # If statement ensures there is input in both boxes
         if plaintext and key:
+            # Key should be 16 bytes, repeats key until it reaches 16
             key = repeat_key(key)
             enc = Encryptor(key)
             # have to encode plaintext and key to turn them into bytes
             cipherText = enc.encrypt(plaintext.encode(), key.encode())
             cipherTextHex = cipherText.hex()
-    return render_template('encrypt.html', cipherText=cipherTextHex)
+    return render_template('encrypt.html', cipherText=cipherTextHex, active_page="encrypt")
 
 @app.route('/decrypt', methods=["POST", "GET"])
 def decrypt():
@@ -31,15 +31,19 @@ def decrypt():
     if request.method == "POST":
         cipherText = request.form["ciphertext"]
         key = request.form["key"]
-        # If statement ensures there is input in both boxes
-        if cipherText and key:
+        # If statement ensures there is input in both boxes and
+        # checks if ciphertext is valid (has to be a multiple of 16)
+        if (cipherText and key):
             key = repeat_key(key)
-        enc = Encryptor(key)
-        # Have to convert cipherText back into a bytes object
-        plainText = enc.decrypt(bytes.fromhex(cipherText), key.encode())
-        # Reverts plainText back from a bytes object to a String object
-        return render_template('decrypt.html', plainText=plainText.decode())
-    return render_template('decrypt.html')
+            enc = Encryptor(key)
+            if len(cipherText) % 16 == 0:
+                # Have to convert cipherText back into a bytes object
+                plainText = enc.decrypt(bytes.fromhex(cipherText), key.encode())
+                # Reverts plainText back from a bytes object to a String object
+                return render_template('decrypt.html', plainText=plainText.decode(), active_page="decrypt")
+            else:
+                return render_template('decrypt.html', plainText="Invalid ciphertext length, must be a multiple of 16", active_page="decrypt")
+    return render_template('decrypt.html', active_page="decrypt")
 
 if __name__ == "__main__":
     app.run(debug=True)
